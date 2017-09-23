e2f367910c1d2eb0ba0d077e779294b0cf6b28ca
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-23 13:36:23 +0200

    Write first version of game that only asks for input and prints it out

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello in my guessing game!");
+
+    println!("Input your guess, please:");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line!");
+
+    println!("Your guess is: {}", guess);
 }

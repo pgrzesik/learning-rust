95736c2d4904cbf320da4dac5becb92491f6f440
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-23 13:50:29 +0200

    Add rand crate and generate random number in game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,21 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Hello in my guessing game!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Input your guess, please:");
 
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line!");
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line!");
 
     println!("Your guess is: {}", guess);
 }

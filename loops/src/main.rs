9af768e1eb1513f4b5a02aa8ee98fb75c166de49
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-24 21:30:22 +0200

    Example of countdown while loop

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,5 +1,21 @@
 fn main() {
+    while_loop(5);
+}
+
+fn infinite_loop() {
     loop {
         println!("Hello, world!");
     }
+}
+
+fn while_loop(x: i32) {
+    let mut number = x;
+
+    while number != 0 {
+        println!("{}!", number);
+
+        number = number - 1;
+    }
+
+    println!("Looping done!");
 }

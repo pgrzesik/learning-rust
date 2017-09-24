57c19a896edc158d04ef5aa1b47724d00568b6fd
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-24 21:44:09 +0200

    Implement converting from fahrenheit to celsius

--- fahrenheit_to_celsius/src/main.rs
+++ fahrenheit_to_celsius/src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter value in Fahrenheit:");
+
+    let mut f = String::new();
+
+    io::stdin().read_line(&mut f)
+            .expect("Failed to read line!");
+
+    let f: f32 = f.trim().parse().expect("Enter valid Fahrenheit value!");
+
+    let c = (f - 32.0) * (5.0/9.0);
+
+    println!("Fahrenheit: {}", f);
+    println!("Celsius: {:.2}", c);
 }

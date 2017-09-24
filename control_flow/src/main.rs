0afd1b4f138123e0759128b0e7b33f4803fe33dc
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-24 21:23:22 +0200

    Multiple if statements example

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
-    let x = 3;
+    let x = 9;
 
     if x < 4 {
         println!("Number less than 4!");
+    } else if x > 10 {
+        println!("Number greater than 10!");
     } else {
-        println!("Number equal to or greater than 4!");
+        println!("Number between 4 and 10!");
     }
 }

c19d8b2fe7c6c29ee96320b733a585a4ecb78f51
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-24 21:45:32 +0200

    Refactor fahrenheit to celsius conversion to function

--- fahrenheit_to_celsius/src/main.rs
+++ fahrenheit_to_celsius/src/main.rs
@@ -10,8 +10,12 @@ fn main() {
 
     let f: f32 = f.trim().parse().expect("Enter valid Fahrenheit value!");
 
-    let c = (f - 32.0) * (5.0/9.0);
+    let c = f_to_c(f);
 
     println!("Fahrenheit: {}", f);
     println!("Celsius: {:.2}", c);
+}
+
+fn f_to_c(f: f32) -> f32 {
+    (f - 32.0) * (5.0/9.0)
 }

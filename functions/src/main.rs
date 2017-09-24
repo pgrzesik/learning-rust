03e350f01f0c65f27b4a5357dc61e24ff2b73d55
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-24 21:14:31 +0200

    Example of using function returning a value

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -3,6 +3,9 @@ fn main() {
 
     another();
     another_with_param(10);
+
+    let x = returning_value();
+    println!("Value returned is equal to: {}", x);
 }
 
 fn another() {
@@ -11,4 +14,8 @@ fn another() {
 
 fn another_with_param(x: i32) {
     println!("Value of passed variable is: {}", x);
+}
+
+fn returning_value() -> i32 {
+    10
 }

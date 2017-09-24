3d3fbb789f6c3c6c10ce871608cd02257c5307a1
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-24 21:10:55 +0200

    Example of using function with parameter

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,8 +2,13 @@ fn main() {
     println!("Hello, world!");
 
     another();
+    another_with_param(10);
 }
 
 fn another() {
     println!("Hello from another function!");
+}
+
+fn another_with_param(x: i32) {
+    println!("Value of passed variable is: {}", x);
 }

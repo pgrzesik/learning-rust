1e6b2554d3af6393b122338e1fb03a900202a31b
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-24 21:01:24 +0200

    Example of tuple usage

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,6 +3,12 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
     const MAX_POINTS: u32 = 100_000;
     println!("The value of contant MAX_POINTS is: {}", MAX_POINTS);
+
+    let tup = (500, 6.4, 1);
+
+    let (a, b, c) = tup;
+    println!("The value of a is: {}", a);
 }

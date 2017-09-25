c4fb0a80df8c8dd6500aba7667a557d41e0a6a9f
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-25 21:21:24 +0200

    Rewrite function to accept string slice as param so it can handle Strings and string literals

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     let s = String::from("Hello there");
 
-    let first = first_word(&s);
+    let first = first_word(&s[..]);
     println!("First word is: {}", first);
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {

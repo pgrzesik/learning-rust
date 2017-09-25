09c44ddb10b3b733a455c7e26fcdd1250ad75e72
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-25 20:28:44 +0200

    Example of using reference instead of passing ownership

--- references_and_borrowing/src/main.rs
+++ references_and_borrowing/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("Hello there!");
+
+    // We're using reference here so the s is not invalidated
+    let len = calculate_length(&s);
+    println!("The length of {} is {}.", s, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }

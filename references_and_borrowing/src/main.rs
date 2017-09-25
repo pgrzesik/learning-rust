90e8692a1d171d3b3923b84395aae0f132eb7764
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-25 20:35:58 +0200

    Example of mutating something that we're referencing

--- references_and_borrowing/src/main.rs
+++ references_and_borrowing/src/main.rs
@@ -4,8 +4,16 @@ fn main() {
     // We're using reference here so the s is not invalidated
     let len = calculate_length(&s);
     println!("The length of {} is {}.", s, len);
+
+    let mut s_mut = String::from("Hello from mutable!");
+    change(&mut s_mut);
+    println!("s_mut: {}", s_mut);
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+fn change(s: &mut String) {
+    s.push_str(" Pushed from func!");
 }

9b4529711c30b1079ba647e69e2adf9c4c83f1c5
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-25 20:19:29 +0200

    Example of copy and moving when it comes to functions

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -16,4 +16,26 @@ fn main() {
 
     // s is no longer valid here!
     // println!("{}", s); would fail
+    // To preserve we can use Copy trait
+
+    let s_to_own = String::from("Some stuff!");
+    takes_ownership(s_to_own);
+
+    // s_to_own is no longed valid here since it was moved to func
+    // println!("{}", s_to_own); fails here
+
+    let x = 10;
+    makes_copy(x);
+
+    // x was copied to function so it's still avaialable in this scope
+
+}
+
+
+fn takes_ownership(owned_string: String) {
+    println!("{}", owned_string);  // After the function ends, owned_string goes out of scope
+}
+
+fn makes_copy(copied_int: i32) {
+    println!("{}", copied_int); // copied_int goes out of scope - nothing special happens
 }

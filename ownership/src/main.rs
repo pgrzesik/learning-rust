65689b05b7d0c747a118ae7174009dc774b0ac60
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-25 20:07:20 +0200

    Add example of invalidating

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello there!");
+
+    // append to string
+
+    s.push_str(" Nice to meet you!");
+
+    println!("{}", s);
+
+    let s2 = s;
+    // s is no longer valid here!
+    println!("{}", s);
 }

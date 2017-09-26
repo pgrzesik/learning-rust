4944dd4b8f8e0a51cddb66378d157128c84d298e
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-26 22:35:20 +0200

    Add implementation with struct usage

--- 13_rectangle_area/src/main.rs
+++ 13_rectangle_area/src/main.rs
@@ -14,6 +14,15 @@ fn main() {
         rect.0, rect.1, area_with_tuple(rect)
     );
 
+    let rect_struct = Rectangle{
+        length,
+        width
+    };
+
+    println!(
+        "Area of rectangle with length: {} and width: {} is equal to: {}",
+        rect_struct.length, rect_struct.width, area_with_struct(&rect_struct)
+    );
 }
 
 fn area(length: u32, width: u32) -> u32 {
@@ -22,4 +31,13 @@ fn area(length: u32, width: u32) -> u32 {
 
 fn area_with_tuple(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
+}
+
+fn area_with_struct(rect: &Rectangle) -> u32 {
+    rect.length * rect.width
+}
+
+struct Rectangle {
+    length: u32,
+    width: u32
 }

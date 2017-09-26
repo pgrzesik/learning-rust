59567fd302b9af294cd9e1f93b5daadcc1c24436
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-26 22:30:46 +0200

    Naive implementation of area rectangle

--- 13_rectangle_area/src/main.rs
+++ 13_rectangle_area/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let length = 50;
+    let width = 30;
+
+    println!(
+        "Area of rectangle with length: {} and width: {} is equal to: {}",
+        length, width, area(length, width)
+    );
+}
+
+fn area(length: u32, width: u32) -> u32 {
+    length * width
 }

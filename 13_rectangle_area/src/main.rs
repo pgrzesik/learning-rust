00d55bb18eb6eddbf051182ae6d12debff3704d0
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-26 22:33:04 +0200

    Add implementation with tuple usage

--- 13_rectangle_area/src/main.rs
+++ 13_rectangle_area/src/main.rs
@@ -6,8 +6,20 @@ fn main() {
         "Area of rectangle with length: {} and width: {} is equal to: {}",
         length, width, area(length, width)
     );
+
+    let rect = (length, width);
+
+    println!(
+        "Area of rectangle with length: {} and width: {} is equal to: {}",
+        rect.0, rect.1, area_with_tuple(rect)
+    );
+
 }
 
 fn area(length: u32, width: u32) -> u32 {
     length * width
+}
+
+fn area_with_tuple(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }

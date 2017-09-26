b40c962d17425ae7efb6a6ffa80c9cfb1cc385eb
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-26 22:17:01 +0200

    Example of struct initialization and field access

--- 12_structs/src/main.rs
+++ 12_structs/src/main.rs
@@ -1,5 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let mut user_instance = User {
+        username: String::from("userrrr"),
+        email: String::from("mail@mail.com"),
+        sign_in_count: 0,
+        active: true
+    };
+
+    user_instance.sign_in_count = 1;
+
+    println!("User sign in count: {}", user_instance.sign_in_count);
 }
 
 

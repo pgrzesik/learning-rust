04a2020685282c7378b6082e6a9b845436b3c079
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-27 16:30:04 +0200

    Example of enum variants with associated String values

--- 14_enums/src/main.rs
+++ 14_enums/src/main.rs
@@ -11,6 +11,10 @@ fn main() {
         kind: IpAddrKind::V6,
         address: String::from("::1")
     };
+
+    let home_enum = IpAddrEnum::V4(String::from("127.0.0.1"));
+
+    let loopback_enum = IpAddrEnum::V6(String::from("::1"));
 }
 
 enum IpAddrKind {
@@ -21,4 +25,9 @@ enum IpAddrKind {
 struct IpAddr {
     kind: IpAddrKind,
     address: String
+}
+
+enum IpAddrEnum {
+    V4(String),
+    V6(String)
 }

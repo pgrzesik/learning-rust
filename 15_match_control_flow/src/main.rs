3174e7d68b28061515466eebd6923b5af0fe16d6
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-27 16:48:40 +0200

    Example of using variants that also hold value with match

--- 15_match_control_flow/src/main.rs
+++ 15_match_control_flow/src/main.rs
@@ -1,13 +1,22 @@
 fn main() {
     println!("Value in cents of Penny: {}", value_in_cents(Coin::Penny));
+    println!(
+        "Value in cents of Quarter: {}",
+        value_in_cents(Coin::Quarter(UsState::California)));
 }
 
+#[derive(Debug)]
+enum UsState {
+    California,
+    Georgia,
+    Texas
+}
 
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter
+    Quarter(UsState)
 }
 
 fn value_in_cents(coin: Coin) -> u32 {
@@ -15,6 +24,9 @@ fn value_in_cents(coin: Coin) -> u32 {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("Quarter from state: {:?}", state);
+            25
+        },
     }
 }

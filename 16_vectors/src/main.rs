d232e7a537a84223bd9e046da4dfcef056095a1b
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-29 20:52:43 +0200

    Example of updating a mutable vector

--- 16_vectors/src/main.rs
+++ 16_vectors/src/main.rs
@@ -3,4 +3,9 @@ fn main() {
 
     // Macro usage for vector
     let v_from_macro = vec![1, 2, 3];
+
+    let mut mutable_vector = Vec::new();
+
+    mutable_vector.push(1);
+    mutable_vector.push(2);
 }

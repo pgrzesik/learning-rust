09e38859f26f13fe5d90e5daa15f75932b0f58bf
Piotr Grzesik <pj.grzesik@gmail.com>
2017-09-29 21:18:14 +0200

    Two examples of getting a reference to vector element

--- 16_vectors/src/main.rs
+++ 16_vectors/src/main.rs
@@ -4,6 +4,9 @@ fn main() {
     // Macro usage for vector
     let v_from_macro = vec![1, 2, 3];
 
+    let third: &i32 = &v_from_macro[2];
+    let third_2: Option<&i32> = v_from_macro.get(2);
+
     let mut mutable_vector = Vec::new();
 
     mutable_vector.push(1);

275bbf1aa74d8c4a47eab193c1464f93e00ed328
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-01 00:14:14 +0200

    Example of initialization of hash map with collect method

--- 18_hash_maps/src/main.rs
+++ 18_hash_maps/src/main.rs
@@ -4,4 +4,9 @@ fn main() {
     let mut scores = HashMap::new();
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let different_scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
 }

a33f08dfa8096cc1ca5fa0b246073c6010542190
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-01 00:01:44 +0200

    Example of concatenating strings with + operator

--- 17_strings/src/main.rs
+++ 17_strings/src/main.rs
@@ -10,4 +10,10 @@ fn main() {
     s.push_str("pushed");
 
     s.push('l');
+
+    let s1 = String::from("First");
+    let s2 = String::from("Second");
+
+    let s3 = s1 + &s2;  // s1 is no longer valid
+
 }

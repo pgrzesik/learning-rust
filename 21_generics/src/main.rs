a7c59802cbe0477fc0a61d0b722264c57e9878aa
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-01 20:39:52 +0200

    Starting point - program for finding largest number in a vector

--- 21_generics/src/main.rs
+++ 21_generics/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let numbers = vec![1, 3, 2, 5, 0];
+
+    let mut largest = numbers[0];
+
+    for number in numbers {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
 }

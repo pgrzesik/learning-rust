05b1443c71be77e29d6ea322848da57a0429f657
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-01 19:48:16 +0200

    Usage of "?" for propagating errors

--- 20_recoverable_errors/src/main.rs
+++ 20_recoverable_errors/src/main.rs
@@ -43,3 +43,10 @@ fn read_username_from_file() -> Result<String, io::Error> {
         Err(e) => Err(e),
     }
 }
+
+fn read_username_from_file_shorthand() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}

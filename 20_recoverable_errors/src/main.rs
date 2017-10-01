6b049508c12e8af916634bf71ebfc112e69e9fce
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-01 19:44:15 +0200

    Example of how function can propagate error up the call stack

--- 20_recoverable_errors/src/main.rs
+++ 20_recoverable_errors/src/main.rs
@@ -24,3 +24,20 @@ fn main() {
     // Similar to unwrap but it's possible to provide panic message
     let f_expect = File::open("hello.txt").expect("Failed to open file hello.txt");
 }
+
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}

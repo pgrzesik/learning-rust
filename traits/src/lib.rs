c6512a5d81c7607e2cab37199cde898119ec898f
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-01 20:57:27 +0200

    Add struct NewsArticle and implement Summarizable trait for it

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -2,6 +2,19 @@ pub trait Summarizable {
     fn summary(&self) -> String;
 }
 
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summarizable for NewsArticle {
+    fn summary(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]

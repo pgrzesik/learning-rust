cf23c92502eceeb391834ef2cdf9b7fb7fb0ee96
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-01 19:35:44 +0200

    Example of handling different types of errors

--- 20_recoverable_errors/src/main.rs
+++ 20_recoverable_errors/src/main.rs
@@ -1,12 +1,21 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     let f = File::open("hello.txt");
 
     let f = match f {
         Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => {
+            match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => {
+                    panic!("File creation failed: {:?}", e)
+                }
+            }
+        },
         Err(error) => {
             panic!("Problem with opening file: {:?}", error)
-        },
+        }
     };
 }

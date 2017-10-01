b82d221df12cd9a7519cfb8ce492fe2e3358ea71
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-01 20:42:38 +0200

    Refactor to function

--- 21_generics/src/main.rs
+++ 21_generics/src/main.rs
@@ -1,13 +1,19 @@
-fn main() {
-    let numbers = vec![1, 3, 2, 5, 0];
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
 
-    let mut largest = numbers[0];
-
-    for number in numbers {
-        if number > largest {
-            largest = number;
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
         }
     }
 
-    println!("The largest number is {}", largest);
+    largest
+}
+
+fn main() {
+    let numbers = vec![1, 3, 2, 5, 0];
+
+    let result = largest(&numbers);
+
+    println!("The largest number is {}", result);
 }

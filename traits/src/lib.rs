c96bdc580f22d156ba1faa75c7d3187edf16c60f
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-01 20:58:58 +0200

    Add struct Tweet and implement Summarizable trait for it

--- traits/src/lib.rs
+++ traits/src/lib.rs
@@ -15,6 +15,19 @@ impl Summarizable for NewsArticle {
     }
 }
 
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summarizable for Tweet {
+    fn summary(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]

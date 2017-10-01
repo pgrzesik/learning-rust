3e0692bcdb46cefc1b646f91d9a6ae2dfc7f6632
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-01 21:27:13 +0200

    Example of method with explicit lifetimes

--- 22_lifetimes/src/main.rs
+++ 22_lifetimes/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("abc");
+    let s2 = "defg";
+
+    let result = longest(s1.as_str(), s2);
+    println!("Longest result is: {}", result);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }

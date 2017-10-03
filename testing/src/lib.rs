0226359e632311b08e24d12906e0bebedf0095ab
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-03 21:16:57 +0200

    Add test which uses custom error message in assert

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -14,6 +14,10 @@ pub fn add_two(x: i32) -> i32 {
     x + 2
 }
 
+pub fn greeting(name: &str) -> String{
+    format!("Hello, {}!", name)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -37,5 +41,14 @@ mod tests {
     #[test]
     fn it_adds_two() {
         assert_eq!(4, add_two(2))
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("John");
+        assert!(
+            result.contains("John"),
+            "Greeting did not contain John, value was `{}`", result
+        );
     }
 }

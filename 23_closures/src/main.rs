fd7739c9d4d9b9e5fe449e80ad2b27f2215c3603
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-07 22:53:21 +0200

    Implement function that will simulate expensive calculation

--- 23_closures/src/main.rs
+++ 23_closures/src/main.rs
@@ -1,3 +1,13 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    simulated_expensive_calculation(2);
+}
+
+
+fn simulated_expensive_calculation(intensity: i32) -> i32 {
+    println!("Very expensive calculation...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
 }

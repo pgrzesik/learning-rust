4a6b873978ea688041aeb29b3230ff64f83d4375
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-08 21:11:17 +0200

    Example of using map

--- 24_iterators/src/main.rs
+++ 24_iterators/src/main.rs
@@ -5,4 +5,7 @@ fn main() {
     for val in some_vec_iter {
         println!("Value: {}", val);
     }
+
+    let some_other_vec: Vec<_> = some_vec.iter().map(|x| x + 1).collect();
+    println!("{:?}", some_other_vec);
 }

2627286e5c77f059dd28e1aed9b3acdea0c2a485
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-08 21:15:31 +0200

    Example of filter

--- 24_iterators/src/main.rs
+++ 24_iterators/src/main.rs
@@ -8,4 +8,27 @@ fn main() {
 
     let some_other_vec: Vec<_> = some_vec.iter().map(|x| x + 1).collect();
     println!("{:?}", some_other_vec);
+
+    let shoes = vec![
+        Shoe { size: 40, style: String::from("sandal") },
+        Shoe { size: 42, style: String::from("sneaker") }
+    ];
+
+    let in_my_size = shoes_in_my_size(shoes, 42);
+
+    for val in in_my_size {
+        println!("In my size! {:?}", val);
+    }
+}
+
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: i32,
+    style: String
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: i32) -> Vec<Shoe> {
+    shoes.into_iter()
+        .filter(|s| s.size == shoe_size)
+        .collect()
 }

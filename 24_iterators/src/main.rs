97da44e42a1619826cbc89b36c6196aecdbc3937
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-08 21:05:52 +0200

    Example of iterator usage in for-loop

--- 24_iterators/src/main.rs
+++ 24_iterators/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let some_vec = vec![1, 2, 3];
+    let some_vec_iter = some_vec.iter();
+
+    for val in some_vec_iter {
+        println!("Value: {}", val);
+    }
 }

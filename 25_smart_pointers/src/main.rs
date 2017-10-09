a48f9df3936bb962b686906d94756c5b0fd5f429
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-09 23:25:32 +0200

    Example of using Deref

--- 25_smart_pointers/src/main.rs
+++ 25_smart_pointers/src/main.rs
@@ -1,13 +1,36 @@
 use List::{Cons, Nil};
+use std::ops::Deref;
 
 fn main() {
     let boxed = Box::new(5);
     println!("boxed = {}", boxed);
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Nil))));
+
+    let song = Mp3 {
+        audio: vec![1, 2, 3, 4, 5],
+        artist: Some(String::from("Travis Scott")),
+        title: Some(String::from("RaRa"))
+    };
+
+    assert_eq!(*song, vec![1, 2, 3, 4, 5]);
 }
 
 enum List {
     Cons(i32, Box<List>),
     Nil
+}
+
+struct Mp3 {
+    audio: Vec<u8>,
+    artist: Option<String>,
+    title: Option<String>
+}
+
+impl Deref for Mp3 {
+    type Target = Vec<u8>;
+
+    fn deref(&self) -> &Vec<u8> {
+        &self.audio
+    }
 }

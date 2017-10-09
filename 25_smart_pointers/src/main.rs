b8de6b2bb8304e9e93350c7b9c7828fe7a11150a
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-09 23:17:35 +0200

    Example of using cons

--- 25_smart_pointers/src/main.rs
+++ 25_smart_pointers/src/main.rs
@@ -1,4 +1,13 @@
+use List::{Cons, Nil};
+
 fn main() {
     let boxed = Box::new(5);
     println!("boxed = {}", boxed);
+
+    let list = Cons(1, Box::new(Cons(2, Box::new(Nil))));
+}
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil
 }

736b31de32b0fa47cf89f509dcf2c13cb48bbfb5
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-10 20:46:34 +0200

    Create channel and spawn a thread that sends string through it

--- 27_message_passing/src/main.rs
+++ 27_message_passing/src/main.rs
@@ -1,3 +1,11 @@
+use std::thread;
+use std::sync::mpsc;
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("Hello there");
+        tx.send(val).unwrap();
+    });
 }

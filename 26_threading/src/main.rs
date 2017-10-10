7655e4e15cd713941a6ea2595bcf4ccf9c728d3b
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-10 20:38:33 +0200

    Example of running loop in main and spawned thread and waiting for spawned thread to end

--- 26_threading/src/main.rs
+++ 26_threading/src/main.rs
@@ -1,3 +1,15 @@
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("{} from spawned thread", i);
+        }
+    });
+
+    for i in 1..5 {
+        println!("{} from main thread", i);
+    }
+
+    handle.join();
 }

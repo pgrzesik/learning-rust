c60092c787e56e7d6bf47488a08b09189e807aaa
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-11 23:18:38 +0200

    Initial structure

--- blog/src/main.rs
+++ blog/src/main.rs
@@ -1,3 +1,15 @@
+extern crate blog;
+use blog::Post;
+
 fn main() {
-    println!("Hello, world!");
+    let mut post = Post::new();
+
+    post.add_text("Sample text");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("Sample text", post.content());
 }
